2fd5b4bfdede132f6176fd2e3fec9f1302904ffe
dilluti0n <hskimse1@gmail.com>
2025-08-10 00:08:00 +0900

    define vite base path to releative to use on electron

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode}) => {
   return {
+    base: './',
     plugins: [
       svelte({
         preprocess: vitePreprocess(),

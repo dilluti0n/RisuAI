cbb028a0663149c3dc7250504191d890a3989535
dilluti0n <hskimse1@gmail.com>
2025-08-16 23:03:04 +0900

    Remove telemetry

--- src/ts/drive/drive.ts
+++ src/ts/drive/drive.ts
@@ -6,7 +6,6 @@ import { BaseDirectory, exists, readFile, readDir, writeFile } from "@tauri-apps
 import { language } from "../../lang";
 import { relaunch } from '@tauri-apps/plugin-process';
 import { sleep } from "../util";
-import { hubURL } from "../characterCards";
 import { decodeRisuSave, encodeRisuSaveLegacy } from "../storage/risuSave";
 
 export async function checkDriver(type:'save'|'load'|'loadtauri'|'savetauri'|'reftoken'){
